3fe6bd20a0be3bb6ba66fafe70fd92435febf8ae
Abrham Dejene <seifu670@gmail.com>
2024-04-24 23:32:59 -0400

    Adding users data

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+const userRouter = require('./routes/user');
 
-app.get('/', (req, res) => {
-  res.send('Sucessful response');
-});
+app.use(express.json());
+
+app.use('/api/v1/user', userRouter);
 
 module.exports = app;
--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+const router = express.Router();
+const User = require('../controllers/user');
+
+router
+  .route('/')
+  .get(User.getAllUsers)
+  .post(User.registerUser);
+router
+  .route('/:id')
+  .get(User.getUser)
+  .put(User.updateUser)
+  .delete(User.deleteUser);
+
+module.exports = router;
