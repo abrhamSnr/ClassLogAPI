adf5d8b6c4041fde10732bc01f07995753d79b7e
Abrham Dejene <seifu670@gmail.com>
2024-05-02 00:20:53 -0400

    Add login user

--- controllers/authUser.js
+++ controllers/authUser.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 const registerUser = async (req, res, next) => {
@@ -17,7 +18,27 @@ const registerUser = async (req, res, next) => {
 const loginUser = async (req, res, next) => {
   try {
     //Use bcrypt and jwt
-    console.log('This is work for tomorrow');
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        message: 'Email or Password is not present'
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: 'User not found please check your email'
+      });
+    }
+    const comparePassowrd = await bcrypt.compare(password, user.password);
+    if (!comparePassowrd) {
+      return res.status(400).json({
+        message: 'Login fail, Please check your email or passowrd'
+      });
+    }
+    res.status(200).json({
+      message: `${user.firstName} ${user.lastName} login succesfully`
+    });
   } catch (err) {
     res.status(400).json({
       message: 'Login fail'
