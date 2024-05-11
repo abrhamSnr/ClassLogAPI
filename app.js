7d87f2ca350f0eaa5045f6dbf4381dabc8cbb5e4
Abrham Dejene <seifu670@gmail.com>
2024-05-10 21:27:07 -0400

    Adding CORs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const userRouter = require('./routes/user');
 const authRouter = require('./routes/authUser');
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/v1/user', userRouter);
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app');
 
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 (async () => {
   try {
@@ -27,5 +27,5 @@ const port = process.env.PORT || 3000;
 })();
 
 app.listen(port, () => {
-  console.log('server is running at port 3000');
+  console.log(`server is running at port ${port}`);
 });
